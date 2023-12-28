d5672dd0f3caee80ec9e686864f7fde1a68afb93
Md Sazzad Hossain <42121775+0xsaju@users.noreply.github.com>
2023-12-28 09:31:01 +0600

    double param in single line url fixed (=param=param)

--- ParamHawk.go
+++ ParamHawk.go
@@ -6,27 +6,29 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"regexp"
 	"strings"
+	"time"
 )
 
 var (
 	targetDomain string
 	collectFlag  bool
 	paramFlag    bool
+	listFlag     bool
 )
 
 func init() {
 	flag.StringVar(&targetDomain, "d", "", "Target domain")
 	flag.BoolVar(&collectFlag, "c", false, "Collect all URLs from the target using waybackurls and store them")
 	flag.BoolVar(&paramFlag, "p", false, "Generate param URLs")
+	flag.BoolVar(&listFlag, "l", false, "Read list of domains from standard input")
 	flag.Parse()
 }
 
 func displayBanner() {
 	fmt.Println(`
    _____                          _    _                _    
-  |  __\\                        | |  | |              | |   
+  |  __ \                        | |  | |              | |   
   | |__) |_ _ _ __ __ _ _ __ ___ | |__| | __ ___      _| | __
   |  ___/ _' | '__/ _' | '_ ' _ \|  __  |/ _' \ \ /\ / / |/ /
   | |  | (_| | | | (_| | | | | | | |  | | (_| |\ V  V /|   < 
@@ -38,32 +40,25 @@ func displayBanner() {
 #              It removes duplicates, trims URLs, and organizes them for further analysis.                      #
 # Author: 0xsaju                                                                                                #
 # LinkedIn: https://linkedin.com/in/0xsaju                                                                      #
-# Version: v_1.0                                                                                                #
+# Version: v_1.1                                                                                                #
 #################################################################################################################
 
 -d      Target domain
--c      Collect all URLs from the target using waybackurls
--p      Generate param URLs
+-c	Collect all URLs from the target using waybackurls
+-p	Generate param URLs
+-l	Read list of domains from standard input
 
 `)
-}
-
-func installWaybackURLs() error {
-	cmd := exec.Command("go", "install", "github.com/tomnomnom/waybackurls@latest")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
 }
 
 func processURL(url string) string {
 	// Check if the URL contains both '?' and '='
 	if strings.Contains(url, "?") && strings.Contains(url, "=") {
-		// Remove all characters after '='
-		re := regexp.MustCompile(`=(.*?)(&|$)`)
-		url = re.ReplaceAllString(url, "=")
+		// Split the URL into two parts: before and after the first '='
+		parts := strings.SplitN(url, "=", 2)
 
-		// Remove duplicate '=' characters
-		url = strings.ReplaceAll(url, "==", "=")
+		// Take only the part before the first '='
+		url = parts[0] + "="
 
 		return url
 	}
@@ -72,11 +67,41 @@ func processURL(url string) string {
 	return ""
 }
 
-func collectURLs(targetDomain string) error {
-	// Install waybackurls as a dependency
-	if err := installWaybackURLs(); err != nil {
-		return fmt.Errorf("error installing waybackurls: %v", err)
+func getOutputFilePath(targetDomain, suffix string) string {
+	// Replace invalid characters in the target domain with underscores
+	targetDomain = strings.ReplaceAll(targetDomain, "://", "")
+	targetDomain = strings.ReplaceAll(targetDomain, "/", "_")
+	targetDomain = strings.ReplaceAll(targetDomain, ".", "_")
+
+	return fmt.Sprintf("%s_%s", targetDomain, suffix)
+}
+
+
+func isWaybackURLsInstalled() bool {
+	cmd := exec.Command("which", "waybackurls")
+	err := cmd.Run()
+	return err == nil
+}
+
+func installWaybackURLs() error {
+	cmd := exec.Command("which", "waybackurls")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Installing waybackurls...")
+		cmd = exec.Command("go", "install", "github.com/tomnomnom/waybackurls@latest")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
 	}
+	fmt.Println("waybackurls is already installed.")
+	return nil
+}
+
+func collectURLs(targetDomain string) error {
+	startTime := time.Now()
+	defer func() {
+		fmt.Printf("Time taken to collect URLs: %s\n", time.Since(startTime))
+	}()
 
 	cmd := exec.Command("waybackurls", targetDomain)
 	output, err := cmd.Output()
@@ -108,6 +133,11 @@ func collectURLs(targetDomain string) error {
 }
 
 func generateParamURLs() error {
+	startTime := time.Now()
+	defer func() {
+		fmt.Printf("Time taken to generate param URLs: %s\n", time.Since(startTime))
+	}()
+
 	filePath := getOutputFilePath(targetDomain, "param_urls.txt")
 	file, err := os.Create(filePath)
 	if err != nil {
@@ -145,36 +175,78 @@ func generateParamURLs() error {
 	return nil
 }
 
-func getOutputFilePath(targetDomain, suffix string) string {
-	// Replace invalid characters in the target domain with underscores
-	targetDomain = strings.ReplaceAll(targetDomain, "://", "")
-	targetDomain = strings.ReplaceAll(targetDomain, "/", "_")
-	targetDomain = strings.ReplaceAll(targetDomain, ".", "_")
-
-	return fmt.Sprintf("%s_%s", targetDomain, suffix)
+func readDomainsFromInput() ([]string, error) {
+	var domains []string
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		domain := scanner.Text()
+		if domain != "" {
+			domains = append(domains, domain)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading domains from standard input: %v", err)
+	}
+	return domains, nil
 }
 
 func main() {
 	displayBanner()
 
-	if targetDomain == "" {
-		fmt.Println("Error: Please provide a target domain using the -d flag.")
-		os.Exit(1)
-	}
-
-	if collectFlag {
-		err := collectURLs(targetDomain)
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
+	if !isWaybackURLsInstalled() {
+		if err := installWaybackURLs(); err != nil {
+			fmt.Printf("Error installing waybackurls: %v\n", err)
 			os.Exit(1)
 		}
 	}
 
-	if paramFlag {
-		err := generateParamURLs()
+	if listFlag {
+		fmt.Println("Reading list of domains from standard input...")
+		domains, err := readDomainsFromInput()
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
+		}
+
+		for _, domain := range domains {
+			targetDomain = domain
+
+			if collectFlag {
+				err := collectURLs(targetDomain)
+				if err != nil {
+					fmt.Printf("Error: %v\n", err)
+					os.Exit(1)
+				}
+			}
+
+			if paramFlag {
+				err := generateParamURLs()
+				if err != nil {
+					fmt.Printf("Error: %v\n", err)
+					os.Exit(1)
+				}
+			}
+		}
+	} else {
+		if targetDomain == "" {
+			fmt.Println("Error: Please provide a target domain using the -d flag.")
+			os.Exit(1)
+		}
+
+		if collectFlag {
+			err := collectURLs(targetDomain)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				os.Exit(1)
+			}
+		}
+
+		if paramFlag {
+			err := generateParamURLs()
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				os.Exit(1)
+			}
 		}
 	}
 }
